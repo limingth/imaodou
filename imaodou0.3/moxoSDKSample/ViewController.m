5913eb323406daa39aa727c8ed92d5d8f02b7aff
Li Ming <limingth@gmail.com>
2014-12-29 12:31:58 -0800

    Modify v0.3 with Stone in Moxtra

--- imaodou0.3/moxoSDKSample/ViewController.m
+++ imaodou0.3/moxoSDKSample/ViewController.m
@@ -28,17 +28,24 @@
 	[self.view addSubview:webView];
 	[webView loadRequest:[NSURLRequest requestWithURL:[NSURL URLWithString:@"http://maodou.io"]]];
     
-    //Add test button on navigator bar
+    //Add start chat button on navigator bar
     self.navigationItem.rightBarButtonItems = [NSArray arrayWithObjects:[[UIBarButtonItem alloc] initWithTitle:@"Start Chat" style:UIBarButtonItemStyleBordered target:self action:@selector(startChat:)],nil];
-    
+
+    //Add login button on navigator bar
+    self.navigationItem.leftBarButtonItems = [NSArray arrayWithObjects:[[UIBarButtonItem alloc] initWithTitle:@"Log in" style:UIBarButtonItemStyleBordered target:self action:@selector(logIn:)],nil];
+
     // Fill in the App Client ID and Client Secret Key received from the app registration step from Moxtra
     // Ruiyun APPID at moxtra.com
     //NSString *APP_CLIENT_ID = @"uGvPYrH651o";
     //NSString *APP_CLIENT_SECRET = @"pIWm8f-u91g";
     
     // limingth@gmail.com  APPID at grouphour.com
-    NSString *APP_CLIENT_ID = @"lu9gyOkcLX0";
-    NSString *APP_CLIENT_SECRET = @"pGSuDGTBSP8";
+    //NSString *APP_CLIENT_ID = @"lu9gyOkcLX0";
+    //NSString *APP_CLIENT_SECRET = @"pGSuDGTBSP8";
+    
+    // limingth@gmail.com  APPID at moxtra.com  NEW
+    NSString *APP_CLIENT_ID = @"qlE_xR7Ruug";
+    NSString *APP_CLIENT_SECRET = @"kY3skOi3CVM";
     
     // Set up Moxtra SDK
     [Moxtra clientWithApplicationClientID:APP_CLIENT_ID applicationClientSecret:APP_CLIENT_SECRET];
@@ -46,36 +53,61 @@
     // Initialize user using unique user identity
     MXUserIdentity *useridentity = [[MXUserIdentity alloc] init];
     useridentity.userIdentityType = kUserIdentityTypeIdentityUniqueID;
-    //useridentity.userIdentityType = kUserIdentityTypeEmail;
+                                                                        //useridentity.userIdentityType = kUserIdentityTypeEmail;
     useridentity.userIdentity = @"limingth";
     
     [[Moxtra sharedClient]
      initializeUserAccount: useridentity
      //orgID: @"maodou.io"
-     orgID: @"P1Nqcw4AEBt6zgOFKGOALh8"      // from admin.grouphour.com, new SDK needs this para
+     //orgID: @"P1Nqcw4AEBt6zgOFKGOALh8"      // from admin.grouphour.com, new SDK needs this para
+     //orgID:nil
+     orgID: @"PLeDKHxCO4kI6uzQCI2cWm1"      // new orgID
      firstName: @"Ming"
      lastName: @"Li"
      avatar: nil
      devicePushNotificationToken: nil
      success: ^{
-         NSLog(@"Setup user account successfully");
+         NSLog(@"Setup user account successfully\n");
      } failure: ^(NSError *error) {
-         NSLog(@"Setup user account failed, %@", [NSString stringWithFormat:@"error code [%d] description: [%@] info [%@]", [error code], [error localizedDescription], [[error userInfo] description]]);
+         NSLog(@"Setup user account failed, %@\n", [NSString stringWithFormat:@"error code [%d] description: [%@] info [%@]", [error code], [error localizedDescription], [[error userInfo] description]]);
      }];
 
     //useridentity.userIdentity = @"user unique identity";
     
 }
 
+- (void)logIn:(id)sender
+{
+    UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Welcome!" message:@"Please enter the meet id:" delegate:self cancelButtonTitle:@"Cancel" otherButtonTitles:nil];
+    alert.alertViewStyle = UIAlertViewStylePlainTextInput;
+    UITextField * alertTextField = [alert textFieldAtIndex:0];
+    alertTextField.keyboardType = UIKeyboardTypeNumberPad;
+    alertTextField.placeholder = @"Enter meet id";
+    [alert addButtonWithTitle:@"Continue"];
+    
+    [alert show];
+}
+
 - (void)startChat:(id)sender
 {
     NSArray *array = [NSArray arrayWithObjects:
-                      @"gaoyancc@gmail.com",nil];
+                      @"limingth0002",nil];
     
-    NSString *conversationID = @"B016RkowP6k1tLQeMRDIrUD";
+    NSString *conversationID = @"B7dd87oA2wIFlF04cQbbulF";
     
-    
-    [[Moxtra sharedClient] inviteMembersWithUniqueIDs: array
+#if 1
+    [[Moxtra sharedClient] createChat:CGRectMake(290, 538, 50, 50)
+                   inviteMembersEmail:nil
+                inviteMembersUniqueID:nil
+                              success:^(NSString *conversationID) {
+                                  NSLog(@"start conversation success, id = %@\n", conversationID);
+                                  //self.conversationID1 = conversationID;
+                                  
+                              } failure:^(NSError *error) {
+                                  NSLog(@"start conversation failed\n");
+                              }];
+#endif
+ /*   [[Moxtra sharedClient] inviteMembersWithUniqueIDs: array
                                           binderID: conversationID
                                            success: ^{
                                                NSLog(@"inviteMembersWithUniqueIDs successfully");
@@ -83,8 +115,9 @@
                                                NSLog(@"inviteMembersWithUniqueIDs failed");
                                            }];
 
-/*
-    [[Moxtra sharedClient] inviteMembersWithEmails: array
+
+
+   [[Moxtra sharedClient] inviteMembersWithEmails: array
                                              binderID: conversationID
                                               success: ^{
                                                   NSLog(@"inviteMembersWithUniqueIDs successfully");
@@ -92,7 +125,7 @@
                                                   NSLog(@"inviteMembersWithUniqueIDs failed");
                                               }];
 */
-    
+#if 0
     [[Moxtra sharedClient] openChat: conversationID
                             withPopupRect: CGRectMake(290, 538, 50, 50)
                               success:^(NSString *conversationID) {
@@ -102,7 +135,7 @@
                               } failure:^(NSError *error) {
                                   NSLog(@"start conversation failed");
                               }];
-    
+#endif
     //    [[Moxtra sharedClient] setDelegate:self];
     
 /*
